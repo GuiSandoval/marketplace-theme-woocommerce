e0c84482fe5e95d18eccabd43e4cec78c52b51f4
Guilherme sandoval <guijpjs@gmail.com>
2020-07-30 20:56:22 -0300

    Categorias na página HOME

--- wp-content/themes/meu_seu_vestido/template-home.php
+++ wp-content/themes/meu_seu_vestido/template-home.php
@@ -32,20 +32,18 @@ Template Name: Home Page
 
                     if ($slider_loop->have_posts()) :
                         while ($slider_loop->have_posts()) :
-                            $slider_loop->the_post();
-
-                    ?>
+                            $slider_loop->the_post();?>
                             <li>
                                 <?php
                                 $imageSlid =  get_the_post_thumbnail_url();
                                 $string = str_replace("-scaled", "", $imageSlid);
 
-                                if ($slider_type[$c] =='image'):
+                                if ($slider_type[$c] == 'image') :
                                 ?>
 
-                                <img src="<?php echo $string; ?>" class="img-fluid wp-post-image" alt="">
+                                    <img src="<?php echo $string; ?>" class="img-fluid wp-post-image" alt="">
 
-                                <?php elseif ($slider_type[$c]== 'image_btn'):?>
+                                <?php elseif ($slider_type[$c] == 'image_btn') : ?>
 
                                     <img src="<?php echo $string; ?>" class="img-fluid wp-post-image" alt="">
                                     <div class="container">
@@ -55,23 +53,23 @@ Template Name: Home Page
                                             </div>
                                         </div>
 
-                                <?php else:?>
-                                    <img src="<?php echo $string; ?>" class="img-fluid wp-post-image" alt="">
-                                    <div class="container">
-                                    <div class="slider-details-container">
-                                        <div class="slider-title">
-                                            <h1><?php the_title(); ?></h1>
-                                        </div>
-                                        <div class="slider-description">
-                                            <div class="subtitle"><?php the_content(); ?></div>
-                                            <a href="<?php echo $slider_button_url[$c]; ?>" class="link"><?php echo $slider_button_text[$c]; ?></a>
-                                        </div>
+                                    <?php else : ?>
+                                        <img src="<?php echo $string; ?>" class="img-fluid wp-post-image" alt="">
+                                        <div class="container">
+                                            <div class="slider-details-container">
+                                                <div class="slider-title">
+                                                    <h1><?php the_title(); ?></h1>
+                                                </div>
+                                                <div class="slider-description">
+                                                    <div class="subtitle"><?php the_content(); ?></div>
+                                                    <a href="<?php echo $slider_button_url[$c]; ?>" class="link"><?php echo $slider_button_text[$c]; ?></a>
+                                                </div>
 
-                                    </div>
-                                </div>
-                                <?php endif;?>
-                                
-                                
+                                            </div>
+                                        </div>
+                                    <?php endif; ?>
+
+
 
                             </li>
                     <?php
@@ -85,34 +83,38 @@ Template Name: Home Page
         </section>
         <section class="novos-produtos">
             <div class="container">
-                <div class="row">Novos Produtos</div>
+                <h2>Novos Produtos</h2>
+                <?php echo do_shortcode('[products limit="4" columns="4" orderby="id" order="DESC" visibility="visible"]'); ?>
             </div>
         </section>
         <section class="destaque-produtos">
-            <div class="container">s
-                <div class="row">Produtos em Destaque</div>
+            <div class="container">
+                <h2>Produtos em Destaque</h2>
+                <hr>
+                <?php echo do_shortcode('[products limit="4" columns="4" visibility="featured" ]'); ?>
             </div>
         </section>
         <section class="popular-products">
             <div class="container">
-                <div class="row">
-                    <?php
-                    if (have_posts()) :
-                        while (have_posts()) : the_post(); ?>
-                            <article>
-                                <h2><?php the_title(); ?></h2>
-                                <div><?php the_content(); ?></div>
-                            </article>
-
-                        <?php endwhile;
-                    else :
-                        ?>
-                        <p>Nada a mostrar</p>
-
-                    <?php endif; ?>
-                </div>
+                <h2>Produtos populares</h2>
+                <hr>
+                <?php echo do_shortcode('[products limit="4" columns="4" best_selling="true" ]'); ?>
             </div>
         </section>
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+                <article>
+                    <h2><?php the_title(); ?></h2>
+                    <div><?php the_content(); ?></div>
+                </article>
+
+            <?php endwhile;
+        else :
+            ?>
+            <p>Nada a mostrar</p>
+
+        <?php endif; ?>
     </main>
 </div>
 <?php get_footer(); ?>
