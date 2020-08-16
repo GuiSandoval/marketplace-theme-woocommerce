271a28635c4b7cb026032a3b31ee202a97034b94
Guilherme sandoval <guijpjs@gmail.com>
2020-08-16 19:38:30 -0300

    Indentação e retirar titulo da My Account

--- wp-content/themes/meu_seu_vestido/page.php
+++ wp-content/themes/meu_seu_vestido/page.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
-    <div class="content-area">
-        <main>
-                <div class="container">
-                    <div class="row">
-                        <?php 
-                        if(have_posts()): 
-                            while(have_posts()): the_post(); ?>
-                            <article class="col">
-                                <h1><?php the_title();?></h1>
-                                <div><?php the_content();?></div>
-                            </article>
+<div class="content-area">
+    <main>
+        <div class="container">
+            <div class="row">
+                <?php
+                if (have_posts()) :
+                    while (have_posts()) : the_post(); ?>
+                        <article class="col">
+                            <?php if (get_the_ID() != 27) : ?>
+                                <h1><?php the_title(); ?></h1>
+                            <?php endif; ?>
+                            <div><?php the_content(); ?></div>
+                        </article>
 
-                        <?php endwhile;
-                        else:
-                        ?>
-                        <p>Nada a mostrar</p>
+                    <?php endwhile;
+                else :
+                    ?>
+                    <p>Nada a mostrar</p>
 
-                        <?php endif;?>
-                    </div>
-                </div>
-        </main>
-    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </main>
+</div>
 <?php get_footer(); ?>
