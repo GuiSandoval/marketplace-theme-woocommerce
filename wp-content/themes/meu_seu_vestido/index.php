e3478dc3b6dfeeeffdfc7cf5c496c7f4513e0567
Guilherme sandoval <guijpjs@gmail.com>
2020-08-16 19:36:31 -0300

    Estilização lista de Posts

--- wp-content/themes/meu_seu_vestido/index.php
+++ wp-content/themes/meu_seu_vestido/index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div class="content-area">
+<div class="content-area mt-5">
     <main>
         <div class="container">
             <div class="row">
@@ -7,12 +7,33 @@
                     <?php
                     if (have_posts()) :
                         while (have_posts()) : the_post(); ?>
-                            <article>
-                                <h2><?php the_title(); ?></h2>
-                                <div><?php the_content(); ?></div>
+                            <article class="bloco-posts" <?php post_class(); ?>>
+                                <h2>
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h2>
+                                <hr>
+                                <div class="post-thumbail">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php if (has_post_thumbnail()) : ?>
+                                            <?php the_post_thumbnail('meu-seu-vestido-slider', array('class' => 'img-fluid mb-3')); ?>
+                                        <?php endif; ?>
+                                    </a>
+                                </div>
+                                <div class="meta">
+                                    <p><b>Publicado por <?php the_author_posts_link(); ?> em <?php echo get_the_date(); ?></b><br>
+                                        <?php if (has_category()) : ?>
+                                            <b>Categorias:<span><?php echo the_category(' '); ?></span></b><br>
+                                        <?php endif; ?>
+                                        <?php if (has_tag()) : ?>
+                                            <b>Tags:<span><?php the_tags('', ', '); ?></span></b></p>
+                                <?php endif; ?>
+                                </div>
+                                <div><?php the_excerpt(); ?></div>
+                                <div class="text-right"><a href="<?php the_permalink();?>">Ler mais(+)</a></div>
                             </article>
 
                         <?php endwhile;
+                            echo bootstrap_pagination();
                     else :
                         ?>
                         <p>Nada a mostrar</p>
